131c38c51f958fe8f32649b2d0c6ca2e47c409e4
Shvier <Shvier1894@gmail.com>
2025-09-06 09:14:40 -0600

    measure flow performance

--- fde-plus/src/veck/elgamal/mod.rs
+++ fde-plus/src/veck/elgamal/mod.rs
@@ -46,14 +46,14 @@ pub mod test {
     use std::cmp::min;
 
     use ark_std::rand::{Rng, SeedableRng};
-    use ark_ff::{PrimeField, BigInteger};
+    use ark_ff::{PrimeField, BigInteger, Zero};
     use ark_poly::{EvaluationDomain, Evaluations, GeneralEvaluationDomain};
     use ark_ec::{pairing::Pairing, Group, CurveGroup};
     use ark_std::{test_rng, UniformRand};
-    use fde::commit::kzg::Powers;
+    use fde::{commit::kzg::Powers, veck::kzg::elgamal::Proof};
     use fde::encrypt::elgamal::MAX_BITS;
 
-    use crate::{veck::elgamal::{read_cipher_from, write_to_file, ElgamalEncryptionProof, KzgElgamalProof}, Scalar, TestCurve, UniPoly};
+    use crate::{veck::{compute_beta, elgamal::{read_cipher_from, write_to_file, ElgamalEncryptionProof, KzgElgamalProof}}, Scalar, TestCurve, UniPoly};
 
     const DATA_SIZE: usize = 32;
     const SUBSET_SIZE: usize = 8;
@@ -151,79 +151,80 @@ pub mod test {
 
     #[test]
     fn flow() {
-        // KZG setup simulation
         let rng = &mut test_rng();
-        let tau = Scalar::rand(rng);
-        let powers = Powers::<TestCurve>::unsafe_setup(tau, (DATA_SIZE + 1).max(MAX_BITS * 4));
 
-        // Server's encryption key for this session
+        let tau = Scalar::rand(rng);
         let encryption_sk = Scalar::rand(rng);
         let encryption_pk = (<TestCurve as Pairing>::G1::generator() * encryption_sk).into_affine();
 
-        // beta
-        let beta = 1.5f64;
-        // number of evaluations to sample
-        let m: usize = ((DATA_SIZE as f64 * beta).ceil() as usize).next_power_of_two();
+        const UPPER_BOUND: usize = 12;
+        let powers = Powers::<TestCurve>::unsafe_setup(tau, (1 << UPPER_BOUND) * 8 + 1);
 
-        // Generate random data and public inputs (encrypted data, etc)
-        let data: Vec<Scalar> = (0..m).map(|_| Scalar::rand(rng)).collect();
-        let m_domain = GeneralEvaluationDomain::new(m).expect("valid domain");
+        const LAMBDA: usize = 128;
+        const SIZE_SUBSET: usize = 1024;
 
-        // Interpolate original polynomial and compute its KZG commitment.
-        // This is performed only once by the server
-        let m_evaluations = Evaluations::from_vec_and_domain(data, m_domain);
-        let f_poly: UniPoly = m_evaluations.interpolate_by_ref();
+        let data_size = 1024;
+
+        let (size_sr, m) = if SIZE_SUBSET > data_size + 1 {
+            (data_size + 1, data_size + 1)
+        } else {
+            let beta = compute_beta(SIZE_SUBSET, LAMBDA);
+            let m = (data_size as f64 * beta).ceil() as usize;
+            (SIZE_SUBSET, m)
+        };
+
+        let mut data: Vec<Scalar> = (0..m).map(|_| Scalar::rand(rng)).collect();
+        let next_pow2 = m.next_power_of_two();
+
+        let pad = next_pow2 - m;
+        if pad > 0 {
+            let pad_evals = vec![Scalar::zero(); pad];
+            data.extend_from_slice(&pad_evals);
+        }
+        let t_start = std::time::Instant::now();
+        println!("Generating encryption proofs ...");
+        let encryption_proof = ElgamalEncryptionProof::new(&data, &encryption_pk, &powers, rng);
+        let elapsed = std::time::Instant::now().duration_since(t_start).as_millis();
+        println!("Generated encryption proofs, elapsed time: {} [ms]", elapsed);
+
+        let domain = GeneralEvaluationDomain::new(next_pow2).expect("valid domain");
+        let index_map = fde::veck::index_map(domain);
+
+        let evaluations = Evaluations::from_vec_and_domain(data, domain);
+        let f_poly: UniPoly = evaluations.interpolate_by_ref();
+        let t_start = std::time::Instant::now();
         let com_f_poly = powers.commit_g1(&f_poly);
+        let elapsed = std::time::Instant::now().duration_since(t_start).as_millis();
+        println!("Commit to f, elapsed time: {} [ms]", elapsed);
 
-        let encryption_proof = ElgamalEncryptionProof::new(&m_evaluations.evals, &encryption_pk, &powers, rng);
-
-        let lambda = 128f64;
-        let order_subset = min(SUBSET_SIZE + 1, (lambda / (beta - 1f64)).ceil() as usize);
-
-        let subdomain_size = order_subset.next_power_of_two();
-
-        println!("m_domain: {}, subset: {}, m_evaluations: {}", m_domain.size(), subdomain_size, m_evaluations.evals.len());
-
-        // Generate the random challenge subset
-        // let mut hasher = Hasher::<TestHash>::new();
-        // hasher.update(&com_f_poly);
-        // let hash_output = hasher.finalize();
-        // let challenge = <TestCurve as Pairing>::ScalarField::from_le_bytes_mod_order(&hash_output);
-        // let seed_vec = challenge.into_bigint().to_bytes_be();
-        // let mut seed_bytes = [0u8; 32];
-        // let bytes = &seed_vec;
-        // let len = bytes.len().min(32);
-        // seed_bytes[..len].copy_from_slice(&seed_vec[..len]);
-        // let mut r_set_rng = StdRng::from_seed(seed_bytes);
-        // let r_set_indices = random_subset_indices(m_domain.size(), subdomain_size, &mut r_set_rng);
-
-        let index_map = fde::veck::index_map(m_domain);
-        let subdomain = GeneralEvaluationDomain::new(subdomain_size).unwrap();
+        let subdomain = GeneralEvaluationDomain::new(size_sr).unwrap();
         let subset_indices = fde::veck::subset_indices(&index_map, &subdomain);
-        let subset_evaluations = fde::veck::subset_evals(&m_evaluations, &subset_indices, subdomain);
+        let subset_evaluations = fde::veck::subset_evals(&evaluations, &subset_indices, subdomain);
 
-        // get subdomain with size suitable for interpolating a polynomial with SUBSET_SIZE
-        // coefficients
-        // let subdomain = GeneralEvaluationDomain::new(subdomain_size).unwrap();
-        // let subset_evaluations = subset_evals(&m_evaluations, &r_set_indices, m_domain);
         let f_s_poly: UniPoly = subset_evaluations.interpolate_by_ref();
         let com_f_s_poly = powers.commit_g1(&f_s_poly);
 
-        let sub_encryption_proof = encryption_proof.subset(&subset_indices);
-        // let vanishing_poly = to_vanishing_poly(subset_indices, m_domain);
+        let t_start = std::time::Instant::now();
+        println!("Start generating range proof");
+        let mut sub_encryption_proof = encryption_proof.subset(&subset_indices);
+        sub_encryption_proof
+                .generate_range_proof(&subset_evaluations.evals, &powers, rng);
+        let elapsed = std::time::Instant::now().duration_since(t_start).as_secs();
+        println!("Generate range proof, elapsed time: {} [s]", elapsed);
 
-        let proof = KzgElgamalProof::new(
+        let proof = Proof::new(
             &f_poly,
             &f_s_poly,
             &encryption_sk,
-            sub_encryption_proof,
+            sub_encryption_proof.clone(),
             &powers,
             rng,
-        )
-        .unwrap();
-        assert!(proof
-            .verify(com_f_poly, com_f_s_poly, encryption_pk, &powers)
-            .is_ok());
+        ).unwrap();
+            
+        let t_start = std::time::Instant::now();
+        assert!(proof.verify(com_f_poly, com_f_s_poly, encryption_pk, &powers).is_ok());
+        let elapsed = std::time::Instant::now().duration_since(t_start).as_secs();
+        println!("Verifying proof, elapsed time: {} [s]", elapsed);
     }
 
     #[test]
